4e8312c3538108076e114e0c66d6d786bf81c94d
Daniel Miller <daniel.miller.djm@gmail.com>
2023-07-06 18:57:19 +1200

    Completed IEventManager implementation

--- Runtime/EventManager.cs
+++ Runtime/EventManager.cs
@@ -1,104 +1,142 @@
 using System;
-using System.Collections.Generic;
 
 namespace DJM.EventManager
 {
-    public class EventManager<TEventId> : IEventManager<TEventId>
+    public class EventManager<TEventId> : EventManagerBase<TEventId>, IEventManager<TEventId>
     {
-        public EventManager()
-        {
-            
-        }
-
         public void AddObserver(TEventId eventId, Action handler)
         {
-            throw new NotImplementedException();
+            AddHandler(eventId, handler, null);
         }
 
         public void AddObserver<TParam>(TEventId eventId, Action<TParam> handler)
         {
-            throw new NotImplementedException();
+            var handlerSignature = new[] { typeof(TParam) };
+            AddHandler(eventId, handler, handlerSignature);
         }
 
         public void AddObserver<TParam1, TParam2>(TEventId eventId, Action<TParam1, TParam2> handler)
         {
-            throw new NotImplementedException();
+            var handlerSignature = new[] { typeof(TParam1), typeof(TParam2) };
+            AddHandler(eventId, handler, handlerSignature);
         }
 
         public void AddObserver<TParam1, TParam2, TParam3>(TEventId eventId, Action<TParam1, TParam2, TParam3> handler)
         {
-            throw new NotImplementedException();
+            var handlerSignature = new[] { typeof(TParam1), typeof(TParam2), typeof(TParam3) };
+            AddHandler(eventId, handler, handlerSignature);
         }
 
         public void AddObserver<TParam1, TParam2, TParam3, TParam4>(TEventId eventId, Action<TParam1, TParam2, TParam3, TParam4> handler)
         {
-            throw new NotImplementedException();
+            var handlerSignature = new[] { typeof(TParam1), typeof(TParam2), typeof(TParam3), typeof(TParam4) };
+            AddHandler(eventId, handler, handlerSignature);
         }
 
         public void RemoveObserver(TEventId eventId, Action handler)
         {
-            throw new NotImplementedException();
+            RemoveHandler(eventId, handler, null);
         }
 
         public void RemoveObserver<TParam>(TEventId eventId, Action<TParam> handler)
         {
-            throw new NotImplementedException();
+            var handlerSignature = new[] { typeof(TParam) };
+            RemoveHandler(eventId, handler, handlerSignature);
         }
 
         public void RemoveObserver<TParam1, TParam2>(TEventId eventId, Action<TParam1, TParam2> handler)
         {
-            throw new NotImplementedException();
+            var handlerSignature = new[] { typeof(TParam1), typeof(TParam2) };
+            RemoveHandler(eventId, handler, handlerSignature);
         }
 
         public void RemoveObserver<TParam1, TParam2, TParam3>(TEventId eventId, Action<TParam1, TParam2, TParam3> handler)
         {
-            throw new NotImplementedException();
+            var handlerSignature = new[] { typeof(TParam1), typeof(TParam2), typeof(TParam3) };
+            RemoveHandler(eventId, handler, handlerSignature);
         }
 
         public void RemoveObserver<TParam1, TParam2, TParam3, TParam4>(TEventId eventId, Action<TParam1, TParam2, TParam3, TParam4> handler)
         {
-            throw new NotImplementedException();
+            var handlerSignature = new[] { typeof(TParam1), typeof(TParam2), typeof(TParam3), typeof(TParam4) };
+            RemoveHandler(eventId, handler, handlerSignature);
         }
 
         public void TriggerEvent(TEventId eventId)
         {
-            throw new NotImplementedException();
+            if (!EventTable.TryGetValue(eventId, out var eventData))
+                return;
+
+            ValidateHandlerSignature(eventId, eventData.HandlerParamSignature, null);
+            
+            var handlers = eventData.Handlers as Action;
+            handlers?.Invoke();
         }
 
         public void TriggerEvent<TParam>(TEventId eventId, TParam param)
         {
-            throw new NotImplementedException();
+            if (!EventTable.TryGetValue(eventId, out var eventData))
+                return;
+            
+            var handlerSignature = new[] { typeof(TParam) };
+            ValidateHandlerSignature(eventId, eventData.HandlerParamSignature, handlerSignature);
+            
+            var handlers = eventData.Handlers as Action<TParam>;
+            handlers?.Invoke(param);
         }
 
         public void TriggerEvent<TParam1, TParam2>(TEventId eventId, TParam1 param1, TParam2 param2)
         {
-            throw new NotImplementedException();
+            if (!EventTable.TryGetValue(eventId, out var eventData))
+                return;
+            
+            var handlerSignature = new[] { typeof(TParam1), typeof(TParam2) };
+            ValidateHandlerSignature(eventId, eventData.HandlerParamSignature, handlerSignature);
+            
+            var handlers = eventData.Handlers as Action<TParam1, TParam2>;
+            handlers?.Invoke(param1, param2);
         }
 
         public void TriggerEvent<TParam1, TParam2, TParam3>(TEventId eventId, TParam1 param1, TParam2 param2, TParam3 param3)
         {
-            throw new NotImplementedException();
+            if (!EventTable.TryGetValue(eventId, out var eventData))
+                return;
+            
+            var handlerSignature = new[] { typeof(TParam1), typeof(TParam2), typeof(TParam3) };
+            ValidateHandlerSignature(eventId, eventData.HandlerParamSignature, handlerSignature);
+            
+            var handlers = eventData.Handlers as Action<TParam1, TParam2, TParam3>;
+            handlers?.Invoke(param1, param2, param3);
         }
 
         public void TriggerEvent<TParam1, TParam2, TParam3, TParam4>(TEventId eventId, TParam1 param1, TParam2 param2, TParam3 param3,
             TParam4 param4)
         {
-            throw new NotImplementedException();
+            if (!EventTable.TryGetValue(eventId, out var eventData))
+                return;
+            
+            var handlerSignature = new[] { typeof(TParam1), typeof(TParam2), typeof(TParam3), typeof(TParam4) };
+            ValidateHandlerSignature(eventId, eventData.HandlerParamSignature, handlerSignature);
+            
+            var handlers = eventData.Handlers as Action<TParam1, TParam2, TParam3, TParam4>;
+            handlers?.Invoke(param1, param2, param3, param4);
         }
 
         public int GetObserverCount(TEventId eventId)
         {
-            throw new NotImplementedException();
+            return EventTable.TryGetValue(eventId, out var eventData)
+                ? eventData.Handlers.GetInvocationList().Length
+                : 0;
         }
 
         public void ClearObservers(TEventId eventId)
         {
-            throw new NotImplementedException();
+            EventTable.Remove(eventId);
         }
 
         public void ClearAllObservers()
         {
-            throw new NotImplementedException();
+            EventTable.Clear();
         }
     }
 }
---
+++ Runtime/EventManagerBase.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DJM.EventManager
+{
+    public abstract class EventManagerBase<TEventId>
+    {
+        private const string HandlerParamSignatureArgumentExceptionFormat = "handler: Handler’s signature does not match the expected signature for event ID {0}. Expected: {1}, Actual: {2}";
+        
+        protected readonly IDictionary<TEventId, EventData> EventTable;
+
+        protected EventManagerBase()
+        {
+            EventTable = new Dictionary<TEventId, EventData>();
+        }
+        
+        
+        // the following methods will throw exceptions if you input incorrect handler params.
+        
+        protected void AddHandler(TEventId eventId, Delegate handler, Type[] handlerParamSignature)
+        {
+            if (EventTable.TryGetValue(eventId, out var eventData))
+            {
+                ValidateHandlerSignature(eventId, eventData.HandlerParamSignature, handlerParamSignature);
+                
+                eventData.Handlers = Delegate.Combine(eventData.Handlers, handler);
+                EventTable[eventId] = eventData;
+            }
+            else
+                EventTable[eventId] = new EventData(handler, handlerParamSignature);
+        }
+        
+        protected void RemoveHandler(TEventId eventId, Delegate handler, Type[] handlerParamSignature)
+        {
+            if (!EventTable.TryGetValue(eventId, out var eventData))
+                return;
+            
+            ValidateHandlerSignature(eventId, eventData.HandlerParamSignature, handlerParamSignature);
+            
+            var remainingHandlers = Delegate.Remove(eventData.Handlers, handler);
+
+            if (remainingHandlers is not null)
+            {
+                eventData.Handlers = remainingHandlers;
+                EventTable[eventId] = eventData;
+                return;
+            }
+            
+            EventTable.Remove(eventId);
+        }
+
+        // protected EventData GetEvent(TEventId eventId)
+        // {
+        //     return EventTable.TryGetValue(eventId, out var eventData) ? eventData : null;
+        // }
+        //
+        // protected void RemoveEvent(TEventId eventId)
+        // {
+        //     EventTable.Remove(eventId);
+        // }
+
+        protected static void ValidateHandlerSignature(TEventId eventId, Type[] expectedTypes, Type[] actualTypes)
+        {
+            // match as no parameters for either
+            if(expectedTypes is null && actualTypes is null) return;
+            
+            // match as neither null, and sequence equal
+            if (expectedTypes != null && actualTypes != null && expectedTypes.SequenceEqual(actualTypes)) return;
+            
+            // no match, throw exception
+            var expected = expectedTypes is null ? "null" : string.Join(", ", expectedTypes.Select(t => t.Name));
+            var actual = actualTypes is null ? "null" : string.Join(", ", actualTypes.Select(t => t.Name));
+                    
+            throw new ArgumentException(string.Format(HandlerParamSignatureArgumentExceptionFormat, eventId, expected, actual));
+        }
+    }
+}
